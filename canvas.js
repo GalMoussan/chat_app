64bd7a2fd859de4d00a1c44aa7787109ddc3d4d3
Gal Moussan <gal@veracityads.com>
2023-01-30 11:33:41 +0200

    changes to url fetching svg

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-import { GlowParticle } from "./glowerparticle"
+import { GlowParticle } from "glowerparticle";
 
 const COLORS = [
 	// { r: 8, g: 88, b: 255 }, // blue
@@ -8,73 +8,73 @@ const COLORS = [
 	{ r: 98, g: 8, b: 1 }, // dark purple
 	{ r: 8, g: 26, b: 255 }, // dark blue rgba(8, 26, 255, 1)
 	{ r: 69, g: 23, b: 176 }, // dark purple rgba(69, 23, 176, 1)
-]
+];
 
 class App {
 	constructor() {
-		this.canvas = document.createElement("canvas")
-		document.body.prepend(this.canvas)
-		this.ctx = this.canvas.getContext("2d")
+		this.canvas = document.createElement("canvas");
+		document.body.prepend(this.canvas);
+		this.ctx = this.canvas.getContext("2d");
 
-		this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1
+		this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
-		this.totalParticles = 15
-		this.particles = []
-		this.maxRadius = 900
-		this.minRadius = 400
+		this.totalParticles = 15;
+		this.particles = [];
+		this.maxRadius = 900;
+		this.minRadius = 400;
 
-		window.addEventListener("resize", this.resize.bind(this), false)
-		this.resize()
+		window.addEventListener("resize", this.resize.bind(this), false);
+		this.resize();
 
-		window.requestAnimationFrame(this.animate.bind(this))
+		window.requestAnimationFrame(this.animate.bind(this));
 	}
 
 	resize() {
-		this.stageWidth = document.body.clientWidth
-		this.stageHeight = document.body.clientHeight
+		this.stageWidth = document.body.clientWidth;
+		this.stageHeight = document.body.clientHeight;
 
-		this.canvas.width = this.stageWidth * this.pixelRatio
-		this.canvas.height = this.stageHeight * this.pixelRatio
-		this.ctx.scale(this.pixelRatio, this.pixelRatio)
+		this.canvas.width = this.stageWidth * this.pixelRatio;
+		this.canvas.height = this.stageHeight * this.pixelRatio;
+		this.ctx.scale(this.pixelRatio, this.pixelRatio);
 
 		// this.ctx.globalCompositeOperation = "saturation"
 
-		this.createParticles()
+		this.createParticles();
 	}
 
 	createParticles() {
-		let curColor = 0
-		this.particles = []
+		let curColor = 0;
+		this.particles = [];
 
 		for (let i = 0; i < this.totalParticles; i++) {
 			const item = new GlowParticle(
 				Math.random() * this.stageWidth,
 				Math.random() * this.stageHeight,
-				Math.random() * (this.maxRadius - this.minRadius) + this.minRadius,
+				Math.random() * (this.maxRadius - this.minRadius) +
+					this.minRadius,
 				COLORS[curColor]
-			)
+			);
 
 			if (++curColor >= COLORS.length) {
-				curColor = 0
+				curColor = 0;
 			}
 
-			this.particles[i] = item
+			this.particles[i] = item;
 		}
 	}
 
 	animate() {
-		window.requestAnimationFrame(this.animate.bind(this))
+		window.requestAnimationFrame(this.animate.bind(this));
 
-		this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight)
+		this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
 		for (let i = 0; i < this.totalParticles; i++) {
-			const item = this.particles[i]
-			item.animate(this.ctx, this.stageWidth, this.stageHeight)
+			const item = this.particles[i];
+			item.animate(this.ctx, this.stageWidth, this.stageHeight);
 		}
 	}
 }
 
-
 window.onload = () => {
-	new App()
-}
+	new App();
+};
--- script.js
+++ script.js
@@ -1,130 +1,130 @@
-import bot from './assets/bot.svg'
-import user from './assets/user.svg'
+import bot from "assets/bot.svg";
+import user from "assets/user.svg";
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
-const textArea = document.querySelector("#textarea")
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+const textArea = document.querySelector("#textarea");
 
-let loadInterval
+let loadInterval;
 
 function loader(element) {
-	element.textContent = ''
+	element.textContent = "";
 
 	loadInterval = setInterval(() => {
 		// Update the text content of the loading indicator
-		element.textContent += '.'
+		element.textContent += ".";
 
 		// If the loading indicator has reached three dots, reset it
-		if (element.textContent === '....') {
-			element.textContent = ''
+		if (element.textContent === "....") {
+			element.textContent = "";
 		}
-	}, 300)
+	}, 300);
 }
 
 function typeText(element, text) {
-	let index = 0
+	let index = 0;
 
 	let interval = setInterval(() => {
 		if (index < text.length) {
-			element.innerHTML += text.charAt(index)
-			index++
+			element.innerHTML += text.charAt(index);
+			index++;
 		} else {
-			clearInterval(interval)
+			clearInterval(interval);
 		}
-	}, 20)
+	}, 20);
 }
 
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
 function generateUniqueId() {
-	const timestamp = Date.now()
-	const randomNumber = Math.random()
-	const hexadecimalString = randomNumber.toString(16)
+	const timestamp = Date.now();
+	const randomNumber = Math.random();
+	const hexadecimalString = randomNumber.toString(16);
 
-	return `id-${timestamp}-${hexadecimalString}`
+	return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
-	return (
-		`
-        <div class="wrapper ${isAi && 'ai'}">
+	return `
+        <div class="wrapper ${isAi && "ai"}">
             <div class="chat">
                 <div class="profile">
                     <img 
                       src=${isAi ? bot : user} 
-                      alt="${isAi ? 'bot' : 'user'}" 
+                      alt="${isAi ? "bot" : "user"}" 
                     />
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-    `
-	)
+    `;
 }
 
 const handleSubmit = async (e) => {
-	e.preventDefault()
+	e.preventDefault();
 
-	const data = textArea.innerText
+	const data = textArea.innerText;
 
 	// user's chatstripe
-	chatContainer.innerHTML += chatStripe(false, data)
+	chatContainer.innerHTML += chatStripe(false, data);
 
-	// to clear the textarea input 
-	textArea.innerText = ""
+	// to clear the textarea input
+	textArea.innerText = "";
 
 	// bot's chatstripe
-	const uniqueId = generateUniqueId()
-	chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+	const uniqueId = generateUniqueId();
+	chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-	// to focus scroll to the bottom 
-	chatContainer.scrollTop = chatContainer.scrollHeight
+	// to focus scroll to the bottom
+	chatContainer.scrollTop = chatContainer.scrollHeight;
 
-	// specific message div 
-	const messageDiv = document.getElementById(uniqueId)
+	// specific message div
+	const messageDiv = document.getElementById(uniqueId);
 
 	// messageDiv.innerHTML = "..."
-	loader(messageDiv)
+	loader(messageDiv);
 
-	const serverURL = window.location.hostname.includes("localhost") ? "http://localhost:5000" : "https://ask-me-anything.onrender.com/"
+	const serverURL = window.location.hostname.includes("localhost")
+		? "http://localhost:5000"
+		: "https://ask-me-anything.onrender.com/";
 
 	const response = await fetch(serverURL, {
-		method: 'POST',
+		method: "POST",
 		headers: {
-			'Content-Type': 'application/json',
+			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({
-			prompt: data
-		})
-	})
+			prompt: data,
+		}),
+	});
 
-	clearInterval(loadInterval)
-	messageDiv.innerHTML = " "
+	clearInterval(loadInterval);
+	messageDiv.innerHTML = " ";
 
-	console.log("RESPPOSNE")
-	console.log(response)
-	console.log(JSON.stringify(response, null, 4))
+	console.log("RESPPOSNE");
+	console.log(response);
+	console.log(JSON.stringify(response, null, 4));
 
 	if (response.ok) {
-		const data = await response.json()
-		const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-		console.log("DATA")
-		console.log(data)
-		console.log(JSON.stringify(data, null, 4))
-		typeText(messageDiv, parsedData)
+		const data = await response.json();
+		const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+		console.log("DATA");
+		console.log(data);
+		console.log(JSON.stringify(data, null, 4));
+		typeText(messageDiv, parsedData);
 	} else {
-		console.log("else ")
-		const err = await response.text()
+		console.log("else ");
+		const err = await response.text();
 
-		messageDiv.innerHTML = "Something went wrong"
-		alert(err)
+		messageDiv.innerHTML = "Something went wrong";
+		alert(err);
 	}
-}
+};
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
 	if (e.keyCode === 13) {
-		handleSubmit(e)
+		handleSubmit(e);
 	}
-})
+});
